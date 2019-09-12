fd9d60b8af559b80053fd266ba515ba201047524
Bhaikko <siddhartha.pawar17@gmail.com>
2019-09-12 23:13:27 +0530

    Folder Restructure done

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import Aux from './../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from './../Backdrop/Backdrop';
 import classes from './Modal.css';
-
-
 
 
 //Built This Way such that <Modal></Modal> can be used to show any elements as part of Modal
