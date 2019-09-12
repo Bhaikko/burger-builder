e6479e3c09f9356ba5b66003df7f4f229f703bab
Bhaikko <siddhartha.pawar17@gmail.com>
2019-09-12 23:03:08 +0530

    Folder Re-structure initial

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,25 +1,39 @@
-import React from 'react';
-
-import classes from './Modal.css';
-
+import React, { Component } from 'react';
 import Aux from './../../../hoc/Aux';
 import Backdrop from './../Backdrop/Backdrop';
+import classes from './Modal.css';
+
+
 
 
 //Built This Way such that <Modal></Modal> can be used to show any elements as part of Modal
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed} />
-        <div 
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }} >
-            {props.children}
-        </div>
-    </Aux>
-    
-);
+class modal extends Component {
+
+    //Optimisation to update modal only if show is updated in BurgerBuilder and due to this, its children also will not update, in this case, Order Summary will also not update
+    shouldComponentUpdate (nextProps, nextState) {
+        return nextProps.show !== this.props.show
+    }
+
+    // componentDidUpdate() {
+    //     console.log("[Modal] updates"); //To check When this component(OrderSummary) updates
+    // }
+
+    render () {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div 
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }} >
+                    {this.props.children}
+                </div>
+            </Aux>
+            
+        );
+    }
+}
 
 export default modal;
