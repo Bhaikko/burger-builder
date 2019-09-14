adb43d7a6857344d756ebf0c27d0f1d183dcf658
Bhaikko <siddhartha.pawar17@gmail.com>
2019-09-14 21:49:15 +0530

    Error handling implemented

--- burger-builder-frontend/src/containers/BurgerBuilder/BurderBuilder.js
+++ burger-builder-frontend/src/containers/BurgerBuilder/BurderBuilder.js
@@ -7,6 +7,7 @@ import BuildControls from './../../components/Burger/BuildControls/BuildControls
 import Modal from './../../components/UI/Modal/Modal';
 import OrderSummary from "./../../components/Burger/OrderSummary/OrderSummary";
 import Spinner from './../../components/UI/Spinner/Spinner'
+import withErrorHandler from './../../hoc/withErrorHandler/withErrorHandler';
 
 import axios from "./../../axios-orders";
 
@@ -21,17 +22,20 @@ class BurgerBuilder extends Component {
     constructor (props) {
         super(props);
         this.state = {
-            ingredients: {
-                salad: 0,
-                bacon: 0,
-                cheese: 0,
-                meat: 0
-            },
+            ingredients: null,
             totalPrice: 4,   //Base Price
             purchasable: false,
             purchasing: false,
             loading: false 
         }
+    }
+
+    componentDidMount () {
+        axios.get("https://burger-builder-96418.firebaseio.com/ingredients.json")
+            .then(response => {
+                this.setState({ ingredients: response.data });
+            })
+            .catch(err => {});
     }
 
     purchaseHandler = () => {
@@ -129,12 +133,36 @@ class BurgerBuilder extends Component {
             disabledInfo[key] = disabledInfo[key] <= 0;
         //  { salad: true, meat: false, ...}
 
-        let orderSummary = <OrderSummary 
-            ingredients={this.state.ingredients}
-            price={this.state.totalPrice}
-            purchaseCancelled={this.purchaseCancelHandler}
-            purchaseContinued={this.purchaseContinueHandler} />
+        let orderSummary = null;
 
+        if(this.state.loading)
+            orderSummary = <Spinner />
+
+        let burger = <Spinner />
+
+        //When ignredients are feteched from Server
+        if(this.state.ingredients)
+        {
+            burger = (
+                <Aux>
+                    <Burger ingredients={this.state.ingredients} />
+                    <BuildControls 
+                        ingredientAdded={this.addIngredientHandler}
+                        ingredientRemoved={this.removeIngredientHandler}
+                        disabled={disabledInfo}
+                        price={this.state.totalPrice}
+                        purchasable={this.state.purchasable}
+                        ordered={this.purchaseHandler} />
+                </Aux>
+            );
+
+            orderSummary = <OrderSummary 
+                ingredients={this.state.ingredients}
+                price={this.state.totalPrice}
+                purchaseCancelled={this.purchaseCancelHandler}
+                purchaseContinued={this.purchaseContinueHandler} />
+        }
+        
         if(this.state.loading)
             orderSummary = <Spinner />
 
@@ -144,17 +172,10 @@ class BurgerBuilder extends Component {
                     {orderSummary}
                 </Modal>
 
-                <Burger ingredients={this.state.ingredients} />
-                <BuildControls 
-                    ingredientAdded={this.addIngredientHandler}
-                    ingredientRemoved={this.removeIngredientHandler}
-                    disabled={disabledInfo}
-                    price={this.state.totalPrice}
-                    purchasable={this.state.purchasable}
-                    ordered={this.purchaseHandler} />
+                {burger}
             </Aux>
         );
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
--- burger-builder-frontend/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger-builder-frontend/src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,5 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const withErrorHandler = (WrappedComponent) => {
-    return (props)
+import Modal from './../../components/UI/Modal/Modal';
+import Aux from './../Aux/Aux'
+
+//Higher Order Function which return a class component
+const withErrorHandler = (WrappedComponent, axios) => { //Axios to use the global error handler
+    return class extends Component { //Inside Function which this will return   and this is anoymouse class without name
+       
+        constructor (props) {
+            super(props);
+
+            //read about interceptors in componentDidMount()
+            this.reqInterceptor = axios.interceptors.request.use(req => {
+                this.setState({  error: null });
+                return req;
+            });
+
+            this.resInterceptor = axios.interceptors.response.use(response => response, error => this.setState({  error: error }));
+
+        }
+        
+        state = {
+            error: null
+        }
+        
+
+        //IMPORTANT
+        // //The componentWillMount() is now deprecated hence the logic inside these functions must be run in constructor() of class component
+        // componentDidMount () {
+        //     //Defined an global interceptor on intial insertion of this component such that it checks for errors
+        //     axios.interceptors.response.use(req => {
+        //         this.setState({  error: null });
+        //         return req;
+        //     });
+
+        //     axios.interceptors.response.use(response => response, error => this.setState({  error: error }));
+        // }
+
+        // componentWillMount () {
+        //     //Defined an interceptor on intial insertion of this component such that it checks for errors
+        //     this.reqInterceptor = axios.interceptors.request.use(req => {
+        //         this.setState({  error: null });
+        //         return req;
+        //     });
+
+        //     this.resInterceptor = axios.interceptors.response.use(response => response, error => this.setState({  error: error }));
+        // }
+
+        //SHIFTED TO constructor ()
+
+        componentWillUnmount () {
+            //Executed when component is no longer required 
+
+            //Removing Interceptors when components are destroyed
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
+        }
+
+        errorConfirmed = () => {
+            this.setState({ error: null });
+        }
+        
+        render () {
+            return (    //JSX the inner function will return
+                <Aux>
+                    <Modal show={this.state.error} modalClosed={this.errorConfirmed}>
+                        {this.state.error ? this.state.error.message : null}
+                    </Modal>
+                    
+                    <WrappedComponent {...this.props} />
+                </Aux>
+            );
+        }
+            
+    }
 }
+
+export default withErrorHandler;
