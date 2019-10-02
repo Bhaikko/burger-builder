aeb2ed27667eb802b84488b58bd4d13f3d001e2f
Bhaikko <siddhartha.pawar17@gmail.com>
2019-10-02 12:00:25 +0530

    Passport added without testing

--- burger-builder-api/api/auth.js
+++ burger-builder-api/api/auth.js
@@ -1,19 +1,12 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const databaseHandler = require("./../database/index");
 const { passport } = require("./../passport");
+const { TOKEN_SECRET_KEY } = require("./../credentials");
 
 const router = express.Router();
-
-const checkLoginStatus = (req, res, next) => {
-    if(!req.user)
-    {
-        res.sendStatus(300);
-        return;
-    }
-    next();
-}
 
 router.get("/", (req, res, next) => {
     res.send("Auth Route Working");
@@ -22,13 +15,41 @@ router.get("/", (req, res, next) => {
 router.post("/signup", (req, res, next) =>{
     bcrypt.hash(req.body.password, 10, function(err, password) {
         databaseHandler.addUser(req.body.email, password)
-            .then(() => res.send("User Added"))
+            .then(() => res.sendStatus(200))
             .catch(err => console.log(err));
     });
 });
 
+
+router.post("/login", {session: false}, (req, res, next) => {
+    passport.authenticate("user", (err, user, info) => {
+        if(err || !user)
+        {
+            return res.status(400).json({
+                message: "Inavlid Email or Password",
+                user: user 
+            });
+        }
+
+        req.login(user, {session: false}, (err) => {
+            if(err) {
+                res.send(err);
+            }
+
+            const token = jwt.sign(user, TOKEN_SECRET_KEY, { expiresIn: '1h' });
+            return res.json({
+                user, 
+                token
+            });
+        });
+    });
+});
+
+// This is responsible for protected requests, and act as middleware
+
+
 router.get("/logout", (req, res, next) => {
-    req.logOut();
+    req.logout();
     res.sendStatus(200);
 });
 
--- burger-builder-api/passport.js
+++ burger-builder-api/passport.js
@@ -1,10 +1,15 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcrypt");
+const passportJWT = require("passport-jwt");
 
+const JWTStrategy = passport.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt; 
 const { Users } = require("./database/database");
+const { TOKEN_SECRET_KEY } = require("./credentials");
 
-passport.use("user", new LocalStrategy((username, password, done) => {
+// This is responsible for the initial login on the web app 
+passport.use("user", new LocalStrategy({ usernameField: "email", passwordField: "password" }, (username, password, done) => {
     Users.findOne({
         where: {
             email: username 
@@ -13,7 +18,7 @@ passport.use("user", new LocalStrategy((username, password, done) => {
         .then(user => {
             if(!user)
                 return done(null, false, { message: "Incorrect Password or Username" });
-            else if(!bcrpyt.compare(password, user.passport, (err, res) => res))
+            else if(!bcrypt.compare(password, user.passport, (err, res) => res))
                 return done(null, false, { message: "Incorrect Password or Username" });
             else 
                 return done(null, user);
@@ -22,17 +27,27 @@ passport.use("user", new LocalStrategy((username, password, done) => {
 }));
 
 
-passport.serializeUser((user, done) => done(null, user.id));
+passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: TOKEN_SECRET_KEY
+}, (jwtPayload, done) => {
+        return Users.findOneById(jwtPayload.id)
+            .then(user => done(null, user))
+            .catch(err => done(err));
+}));
 
-passport.deserializeUser((userId, done) => {
-    Users.findOne({
-        where: {
-            id: userId 
-        }
-    })
-        .then(user => done(null, user))
-        .catch(err => console.log(err));
-});
+
+// passport.serializeUser((user, done) => done(null, user.id));
+
+// passport.deserializeUser((userId, done) => {
+//     Users.findOne({
+//         where: {
+//             id: userId 
+//         }
+//     })
+//         .then(user => done(null, user))
+//         .catch(err => console.log(err));
+// });
 
 module.exports = {
     passport 
